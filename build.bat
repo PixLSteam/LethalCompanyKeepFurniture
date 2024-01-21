0a93d96cac154e080f6e11ddf62ed37b8148db2f
PixLSteam <PixLSteam@users.noreply.github.com>
2024-01-22 00:54:30 +0100

    Added build.bat

---
+++ build.bat
@@ -0,0 +1,2 @@
+del LethalCompanyKeepFurniture.zip
+7z a LethalCompanyKeepFurniture.zip icon.png manifest.json README.md .\LethalCompanyKeepFurniture\bin\Release\net7.0\LethalCompanyKeepFurniture.dll
